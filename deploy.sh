45c749d95085de1fc61c66dfc86202cf1700e25c
edwardyhuang <eddiehuang@hotmail.com>
2021-07-18 15:27:38 +0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t edwardyhuang/multi-client:latest -t edwardyhuang/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t edwardyhuang/multi-server:latest -t edwardyhuang/multi-server:$SHA ./server/Dockerfule ./server
+docker build -t edwardyhuang/multi-worker:latest -t edwardyhuang/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push edwardyhuang/multi-client:latest
+docker push edwardyhuang/multi-server:latest
+docker push edwardyhuang/multi-worker:latest
+
+docker push edwardyhuang/multi-client:$SHA
+docker push edwardyhuang/multi-server:$SHA
+docker push edwardyhuang/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=edwardyhuang/multi-server:$SHA
+kubectl set image deployments/client-deployment client=edwardyhuang/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=edwardyhuang/multi-worker:$SHA
